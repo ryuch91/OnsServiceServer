8f5ac34b5724d7a516a08c8c939b658386b7c40d
ChungkwonRyu <ryuch91@kaist.ac.kr>
2015-12-04 21:12:03 +0900

    add libraries and made server part of communication

--- OnsServiceServer/src/main/ons/ServerMain.java
+++ OnsServiceServer/src/main/ons/ServerMain.java
@@ -1,13 +1,54 @@
 package main.ons;
 
+//imported for logging
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+//imported for communication with android client
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.DataInputStream;
+import java.io.OutputStream;
+import java.io.DataOutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
 
 public class ServerMain{
-	Logger logger = LoggerFactory.getLogger(ServerMain.class);
+	private final static Logger logger = LoggerFactory.getLogger(ServerMain.class);
 	
 	public static void main(String[] args) throws IOException{
+		ServerSocket serverSck = null;
+		Socket clientSck = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		
+		
+		serverSck = new ServerSocket(7777);
+		
+		try{
+			logger.info("Server is listening...");
+			clientSck = serverSck.accept();
+			logger.info("Client is connected.");
+			out = new PrintWriter(clientSck.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSck.getInputStream()));
+			while(true){
+				String inputLine = null;
+				inputLine = in.readLine();
+				logger.info("String from Client : {}",inputLine);
+				if(inputLine.equals("quit"))
+					break;
+			}
+			out.close();
+			in.close();
+			clientSck.close();
+			serverSck.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		
 	}
 	
