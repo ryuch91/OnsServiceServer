5b2397bec9d980a7a05dca019191870a27fde7b8
ChungkwonRyu <ryuch91@kaist.ac.kr>
2015-12-05 18:13:19 +0900

    Change server to thread

--- OnsServiceServer/src/main/ons/ServerMain.java
+++ OnsServiceServer/src/main/ons/ServerMain.java
@@ -5,23 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //imported for communication with android client
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.io.IOException;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.InputStream;
@@ -33,12 +17,12 @@ import java.io.OutputStreamWriter;
 import java.io.DataOutputStream;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.net.UnknownHostException;
 import java.net.SocketException;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-
 import main.ons.JsonBuilder;
 
 public class ServerMain{
@@ -46,51 +30,61 @@ public class ServerMain{
 	
 	private final static int PORT_NUM = 7777;
 	private static JsonBuilder jsonObject=null;
-	private static ServerSocket serverSocket = null;
-	private PrintWriter pw_sock = null;
 	
 	public ServerMain(){
 		jsonObject = new JsonBuilder();
 	}
 	
 	public static void main(String[] args) throws IOException{
-		try{
-			serverSocket = new ServerSocket(PORT_NUM);
-			logger.info("Server is ready...");
-		}catch(IOException e){
-			e.printStackTrace();
-		}
 		
-		while(true){
+		Thread serverThread = new Thread(new TCPServer());
+		serverThread.start();
+	}
+	
+	private static class TCPServer implements Runnable{
+		private ServerSocket serverSocket;
+		public void run(){
 			try{
-				logger.info("Server is listening...");
-				Socket socket = serverSocket.accept();
-				logger.info("{} send connection request", socket.getInetAddress());
+				logger.info("Server: Openning...");
+				serverSocket = new ServerSocket(PORT_NUM);
 				
-				InputStream in = socket.getInputStream();
-				DataInputStream din = new DataInputStream(in);
-				OutputStream out = socket.getOutputStream();
-				DataOutputStream dout = new DataOutputStream(out);
-				
-				String sgtinCode = din.readUTF();
-				logger.info("IP: {} sent sgtin string : {}",socket.getInetAddress(),sgtinCode);
-												
-				//String strJson = jsonObject.jsonToString();
-				String strJson = "Hello!";
-				
-				//<-- here, NAPTR lookup code needs
-				try{
-					dout.writeUTF(strJson);
-				}catch(IOException e){
-					e.printStackTrace();
+				while(true){
+					Socket client = serverSocket.accept();
+					logger.info("Server: Receiving...");
+					logger.info("{} send connection request", client.getInetAddress());
+					try{
+						BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+						String str = in.readLine();
+						logger.info("Server : Received '{}' from client",str);
+						
+						//<-- here, NAPTR lookup code needs
+						
+						//<-- here, Translate it with JSON and send JSON value to client
+						
+						//<JSON USAGE>	
+						//String strJson = jsonObject.jsonToString();
+						//jsonObject.addJson("a", "Hello");
+						//jsonObject.addJson("b", "Hi");
+						//String strJson = jsonObject.jsonToString();
+						
+						PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())),true);
+						out.println("Server received "+str);
+					}catch(Exception e){
+						logger.info("Stream open error");
+						e.printStackTrace();
+					}finally{
+						client.close();
+						logger.info("Server: Done");
+					}
 				}
-				
-				din.close();
-				dout.close();
-				socket.close();
-				logger.info("Connection from {} is closed...",socket.getInetAddress());
-				
 			}catch(IOException e){
+				logger.info("Server socket open error");
+				try {
+					serverSocket.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				e.printStackTrace();
 			}
 		}
